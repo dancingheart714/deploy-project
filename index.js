99e1a702b7cf49398a1dad6cae624fb142e93aa2
Leah King <dancingheart714@yahoo.com>
2021-05-06 16:34:54 -0400

    express app

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ if (process.env.NODE_ENV === 'development') {
   const cors = require('cors');
   server.use(cors());
 }
+
+server.use('*', (req, res) => {
+  res.send('<h1>Success Status Is Granted!</h1>');
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
