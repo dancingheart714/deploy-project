c3592000df7d1630785aea94e659a15112ed6a9c
Leah King <dancingheart714@yahoo.com>
2021-05-06 16:28:38 -0400

    cors

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require('dotenv').config();
+
 console.log("It's working!");
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development') {
+  const cors = require('cors');
+  server.use(cors());
+}
